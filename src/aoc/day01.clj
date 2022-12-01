ff06b51c666a153997fd4ed67f4e784dc60c90b1
Pieter Bruyninckx <pieter.bruyninckx@verint.com>
2022-12-01 16:49:02 +0000

    Day 1 part 2

--- src/aoc/day01.clj
+++ src/aoc/day01.clj
@@ -10,11 +10,19 @@
        (map #(map (fn [x] (Integer/parseInt x)) % ))))
 
 
-(defn solve [v]
+(defn solve1 [v]
   (->> v
        (map #(reduce + %))
        (reduce max)))
 
+(defn solve2 [v]
+  (->> v
+       (map #(reduce + %))
+       (sort >)
+       (take 3)
+       (apply +)))
+
 (defn -main []
   (let [input (parse-input "resources/input01.txt")]
-    (println (solve input))))
+    (println (solve1 input))
+    (println (solve2 input))))
