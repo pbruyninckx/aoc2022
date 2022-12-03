05498f2d8c336b2ee517501fae27c1a46b99be94
Pieter Bruyninckx <pbruyninckx@gmail.com>
2022-12-03 11:36:26 +0000

    Day 3 part 2
    Including some refactoring to ensure as much code as possible
    is shared between part 1 and part 2

--- src/aoc/day03.clj
+++ src/aoc/day03.clj
@@ -13,21 +13,26 @@
       (- i 38)
       (- i 96))))
 
-(defn get-common [s]
-  (let [halves (split-at (/ (count s) 2) s)]
-    (->> halves
-         (map set)
-         (apply set/intersection)
-         first)))
+(defn split-in-halves [s]
+  (split-at (/ (count s) 2) s))
 
-(defn solve [v]
+(defn get-common [backpacks]
+  (->> backpacks
+       (map set)
+       (apply set/intersection)
+       first))
+
+(defn solve [part v]
   (->> v
+       ((if (= part 1)
+          #(map split-in-halves %)
+          #(partition 3 %)))
        (map get-common)
        (map priority)
        (reduce +)))
 
-
 (defn -main []
   (let [input (parse-input "resources/input03.txt")]
-    (println (solve input))))
+    (println (solve 1 input))
+    (println (solve 2 input))))
 
