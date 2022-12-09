48152d46117e4ce2e7f62e58ab850dd87e245d74
Pieter Bruyninckx <pieter.bruyninckx@verint.com>
2022-12-09 11:28:42 +0000

    Update clojure to recent version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aoc2022 "0.0.5"
   :description "My Advent Of Code 2022 solutions"
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/algo.generic "0.1.3"]
                  [org.clojure/data.priority-map "1.1.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
