ebd9798b0ac5bfca6dbb7f18351b07890e587b11
Pieter Bruyninckx <pbruyninckx@gmail.com>
2022-12-10 12:25:12 +0000

    Add match library - not yet needed

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject aoc2022 "0.0.5"
   :description "My Advent Of Code 2022 solutions"
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/core.match "1.0.0"]
                  [org.clojure/algo.generic "0.1.3"]
                  [org.clojure/data.priority-map "1.1.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
