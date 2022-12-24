d8dcb1ac5498a9b98a6f69aeee604093a3e369cd
Pieter Bruyninckx <pbruyninckx@gmail.com>
2022-12-24 14:03:37 +0000

    Day 18 part 2
    Some ugly magic numbers, and it's not very efficient, but it works :)

--- src/aoc/day18.clj
+++ src/aoc/day18.clj
@@ -1,6 +1,7 @@
 (ns aoc.day18
   (:require [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.pprint :as pp]))
 
 (defn parse-input [f]
   (->> f
@@ -14,15 +15,43 @@
             pos (range 3)]
         (update location pos fun))))
 
-(defn solve [locations]
+(defn flatten-1 [xs]
+  (apply concat xs))
+
+(defn extend [locations]
+  (into locations
+        (flatten-1 (map neighbours locations))))
+
+(defn shrink [keep-locations extended-locations]
+  (->> extended-locations
+       (filter (fn [l] (= (count (set/intersection (set (neighbours l))
+                                                   extended-locations))
+                          6)))
+       (into keep-locations)))
+
+(defn fill [locations]
+  (->> locations
+       (iterate extend)
+       (drop 12)
+       first
+       (iterate (partial shrink locations))
+       (drop 20)
+       first))
+
+(defn count-surface [locations]
   (->> locations
        (reduce (fn [[seen num-sides] loc]
                  [(conj seen loc)
-                  (+ num-sides (- (* 2 (count (set/difference (neighbours loc) seen))) 6))])
+                  (+ num-sides
+                     (- (* 2 (count (set/difference (neighbours loc) seen))) 6))])
                [#{} 0])
        second))
 
+(defn count-external-surface [locations]
+  (count-surface (fill (set locations))))
+
 (defn -main []
   (let [locations (parse-input "resources/input18.txt")]
-    (println (solve locations))))
+    (doseq [solve [count-surface count-external-surface]]
+      (println (solve locations)))))
 
