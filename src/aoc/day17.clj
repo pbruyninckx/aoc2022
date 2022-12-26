a6ce2d54ceeeb4ab91148dd90f670572a150608d
Pieter Bruyninckx <pbruyninckx@gmail.com>
2022-12-26 14:13:56 +0000

    Day 17 part 2

--- src/aoc/day17.clj
+++ src/aoc/day17.clj
@@ -49,14 +49,19 @@
 (defn push [direction pos]
   (update pos 1 ({\< dec, \> inc} direction)))
 
-(defn solve [jet-patterns]
+(defn get-tower-top [tower]
+  (if (> (count tower) 10)
+    (subvec tower (- (count tower) 10))))
+
+(defn solve [jet-patterns target-blocks]
   (loop [[block & _ :as blocks] blocks
-         [h x :as block-position] [3 2]
-         [direction & _ :as jet-patterns] jet-patterns
+         block-position [3 2]
+         [[ind-dir direction] & _ :as jet-patterns] jet-patterns
          tower []
-         num-fallen 0]
+         num-fallen 0
+         seen-tops {}]
     (cond
-      (= 2022 num-fallen)
+      (= target-blocks num-fallen)
       (count tower)
       :else
       (let [suggested-position (push direction block-position)
@@ -66,19 +71,32 @@
         (if
           (overlaps? tower block (lower block-position))
           (let [new-tower (add-to-tower tower block block-position)]
-            (recur
-              (rest blocks)
-              [(+ (count new-tower) 3) 2]
-              (rest jet-patterns)
-              new-tower
-              (inc num-fallen)))
+            (let [[old-height old-num-fallen]
+                  (seen-tops [(get-tower-top new-tower) ind-dir (second blocks)])]
+              (if (and old-height
+                       (= 0 (rem (- target-blocks old-num-fallen) (- (inc num-fallen) old-num-fallen))))
+                (+ old-height (* (- (count new-tower) old-height)
+                                 (quot (- target-blocks old-num-fallen)
+                                       (- (inc num-fallen) old-num-fallen))))
+                (recur
+                  (rest blocks)
+                  [(+ (count new-tower) 3) 2]
+                  (rest jet-patterns)
+                  new-tower
+                  (inc num-fallen)
+                  (conj seen-tops [[(get-tower-top new-tower) ind-dir (second blocks)]
+                                   [(count new-tower) (inc num-fallen)]])))))
           (recur
             blocks
             (lower block-position)
             (rest jet-patterns)
             tower
-            num-fallen))))))
+            num-fallen
+            seen-tops))))))
 
 (defn -main []
-  (let [jet-patterns (cycle (parse-input "resources/input17.txt"))]
-    (println (solve jet-patterns))))
+  (let [jet-patterns (->> (parse-input "resources/input17.txt")
+                          (map-indexed vector)
+                          cycle)]
+    (doseq [target-blocks [2022 1000000000000]]
+      (println (solve jet-patterns target-blocks)))))
