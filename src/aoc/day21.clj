413a31ccd49cb7a5a747f8fbca23610874b711fa
Pieter Bruyninckx <pbruyninckx@gmail.com>
2023-01-17 21:27:38 +0100

    Day 21 part 2
    I'm quite happy with using a function as intermediate results,
    as this means the tree only needs to be (explicitly) traversed once.
    
    I also realised that memoising is not necessary for this one,
    as the input is a binary tree.

--- src/aoc/day21.clj
+++ src/aoc/day21.clj
@@ -2,10 +2,12 @@
   (:require [clojure.string :as str]))
 
 (defn str-to-op [s]
-  (get {"/" quot} s (eval (symbol s))))
+  (if (= s "/")
+    quot
+    (eval (symbol s))))
 
 (defn parse-line [line]
-  (let [[target left op right] (str/split line #"[ :]+" )]
+  (let [[target left op right] (str/split line #"[ :]+")]
     (if (re-matches #"\d+" left)
       [target (parse-long left)]
       [target [(str-to-op op) left right]])))
@@ -17,17 +19,45 @@
        (map parse-line)
        (into {})))
 
-(def compute
-  (memoize (fn [operations target]
-             (let [rule (operations target)]
-               (if (int? rule)
-                 rule
-                 (let [[op & args] rule]
-                   (apply op (map (partial compute operations) args))))))))
+(defn compute [operations target]
+  (let [rule (operations target)]
+    (if (number? rule)
+      rule
+      (let [[op & args] rule]
+        (apply op (map (partial compute operations) args))))))
+
+(def associative-op #{+ *})
+(def inverse-op {+ -, - +, * quot, quot *})
+
+(defn compute2 [operations target]
+  "If the result doesn't depend on 'humn' a number is returned,
+  otherwise it's a function that takes the desired output,
+  and returns the required value of 'humn' to get to that output."
+  (let [rule (operations target)]
+    (cond (= target "humn") identity
+          (number? rule) rule
+          :else
+          (let [[op & args] rule
+                [a b :as args] (mapv (partial compute2 operations) args)]
+            (cond
+              (not-any? fn? args)
+              (apply op args),
+              (= target "root")
+              (if (fn? a) (a b) (b a)),
+              (fn? a)
+              (comp a #((inverse-op op) % b)),
+              (associative-op op)
+              (comp b #((inverse-op op) % a)),
+              :else
+              (comp b #(op a %)))))))
 
 (defn solve [operations]
   (compute operations "root"))
 
+(defn solve2 [operations]
+  (compute2 operations "root"))
+
 (defn -main []
   (let [operations (parse-input "resources/input21.txt")]
-    (println (solve operations))))
+    (doseq [solve-fn [solve solve2]]
+      (println (solve-fn operations)))))
